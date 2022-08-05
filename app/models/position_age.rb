5a3f15d4943b848563ba3a1857fa0ad895582b8d
Stacey Stewart <sstewart@movableink.com>
2022-08-05 12:52:55 -0400

    adds method onto PositionAge model for getting the average age

--- app/models/position_age.rb
+++ app/models/position_age.rb
@@ -1,2 +1,8 @@
 class PositionAge < ApplicationRecord
+    def average_age
+        ages_list = self.ages
+
+        numerical_ages = ages_list.map { |age| age.to_i }
+        numerical_ages.sum.fdiv(numerical_ages.size).round
+    end
 end
