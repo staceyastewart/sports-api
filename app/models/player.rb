c43d1ba006e3aab8a036de0b4286815d7ee2bc80
Stacey Stewart <sstewart@movableink.com>
2022-08-08 20:48:17 -0400

    implements search on the Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,23 @@
 class Player < ApplicationRecord
+    include PgSearch::Model
+    pg_search_scope :last_name_starts_with,
+                    against: :last_name,
+                    using: {
+                        tsearch: { prefix: true }
+                    }
+
+    pg_search_scope :search_all,
+                    against: [:age, :position, :sport],
+                    using: [:tsearch]
+
+    def self.search(params)
+        search_all_query = "#{params[:age]} #{params[:position]}, #{params[:sport]})"
+        last_name_query = params[:last_name]
+
+        if last_name_query
+            self.search_all(search_all_query).last_name_starts_with(last_name_query)
+        else
+            self.search_all(search_all_query)
+        end
+    end
 end
